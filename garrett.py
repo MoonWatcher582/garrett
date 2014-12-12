b941f2c03ce42cf6d420cbdbf43bbac25d8631f4
Matt Klein <shesso8bit@gmail.com>
2014-12-11 22:56:44 -0500

    not sure if works for getting to contentwrap

--- garrett.py
+++ garrett.py
@@ -11,5 +11,9 @@ def main():
 	
 	cards = blackjack.find('div',attrs={"class":"content-block content-block-body content-block-note content-block-bg"}).find('div',attrs={"class":"wrap-container clearfix"}).find('div',attrs={"class":"card-container"}).find('div',attrs={"class":"clearfix"}).find('div',attrs={"class":"content-wrap"})
 
+	for card in cards.findAll('div'):
+		for side in card.findAll('div'):
+			print side.find('div',attrs={"class":"side side-text"}).find('div',attrs={"class":"text text-small"})
+
 if __name__ == '__main__':
 	main()
